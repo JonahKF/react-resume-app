5e1500ff65d6ad46317af12393ffec6089281ab8
jonah <jonahkf@gmail.com>
2025-10-07 08:43:16 +0900

    added default styling in index.css, layout in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <header>
+        <h1>Resume Builder</h1>
+      </header>
+
+      <main>
+        <div className="left-pane">
+          {/* Components for General Info, Edu Experience, Practical Experience */}
+        </div>
+        <div className="right-pane">{/* Component for Resume Preview */}</div>
+      </main>
+    </>
+  );
 }
 
 export default App;
